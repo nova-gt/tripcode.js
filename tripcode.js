7a0dbaa735b44c100ded870d8f7a6a508ada958e
nova-gt <nova-gt@users.noreply.github.com>
2017-08-01 00:33:18 +0900

    Divide conversion functions

--- tripcode.js
+++ tripcode.js
@@ -1,4 +1,5 @@
 /*
+2017/07/31 変換処理を整理
 2017/07/29 インデント調整(タブ文字→半角スペース)
 2017/07/28 クロージャ周りを整理
 2017/07/11 ES2015
@@ -33,6 +34,7 @@ sha1.js : http://user1.matsumoto.ne.jp/~goma/js/hash.html
     /** 末尾ピリオドのパターン */
     const tailPeriodsPattern = /\.+$/;
 
+    /** ダイジェストを生キーに変換 */
     const rawKey = (digest, salt) => "##" + digest.toUpperCase() + salt.replace(tailPeriodsPattern, "");
 
     /** Shift_JIS半角カナのバイトパターン */
@@ -62,14 +64,94 @@ sha1.js : http://user1.matsumoto.ne.jp/~goma/js/hash.html
      */
     const toHalfWidthKana = char => {
         const codePoint = char.codePointAt(0);
-            if (0x30 <= codePoint && codePoint <= 0x39) {
-                return String.fromCodePoint(codePoint + 0x71);
-            } else if (0x41 <= codePoint && codePoint <= 0x5A) {
-                return String.fromCodePoint(codePoint + 0x6A);
-            } else if (0x61 <= codePoint && codePoint <= 0x7A) {
-                return String.fromCodePoint(codePoint + 0x64);
-            }
-            return char === "." ? String.fromCodePoint(0xDF) : char;
+        if (0x30 <= codePoint && codePoint <= 0x39) {
+            return String.fromCodePoint(codePoint + 0x71);
+        } else if (0x41 <= codePoint && codePoint <= 0x5A) {
+            return String.fromCodePoint(codePoint + 0x6A);
+        } else if (0x61 <= codePoint && codePoint <= 0x7A) {
+            return String.fromCodePoint(codePoint + 0x64);
+        }
+        return char === "." ? String.fromCodePoint(0xDF) : char;
+    };
+
+    /**
+     * 10桁トリップ生成
+     */
+    const tripcodeWithKey = (key, appendRawKey) => {
+        // saltを準備
+        let salt = "H.";
+        if (key.length > 0) {
+            salt = [...`${key}${salt}`.substr(1, 2)].map(toSalt).join("");
+        }
+
+        // 変換
+        const tripcode = crypt(key, salt);
+
+        // 生キー付加
+        if (appendRawKey) {
+            const digest = [...key]
+                .map(toDigest)
+                .concat(new Array(16).fill("0"))
+                .join("")
+                .substr(0, 16);
+            return tripcode + " " + rawKey(digest, salt);
+        }
+
+        return tripcode;
+    };
+
+    /**
+     * 10桁トリップ生成(生キー)
+     */
+    const tripcodeWithRawKey = (key, append) => {
+        if (key[0] !== "#") return null;
+
+        const result = key.match(rawKeyPattern);
+        if (!result) return "???";
+
+        const digest = result[1];
+        const salt = (result[2] + "..").substr(0, 2);
+
+        // キーをバイナリ文字列に変換
+        key = new Array(8)
+            .fill()
+            .map((v, i) => "%" + digest.substr(i * 2, 2))
+            .join("");
+        key = unescape(key);
+
+        // 変換
+        const tripcode = crypt(key, salt);
+
+        // 生キー付加
+        return append
+            ? tripcode + " " + rawKey(digest, salt)
+            : tripcode;
+    };
+
+    /**
+     * 15桁トリップ生成
+     */
+    const tripcode15WithKey = (key) => {
+        if (key[0] !== "$") return null;
+
+        const tripcode = btoa(sha1.bin(key))
+            .substr(3, 15)
+            .replace("+", ".")
+            .replace("/", "!");
+
+        // キー2文字目が半角カナの場合はトリップを半角カナに置換
+        return key.match(halfWidthKanaPattern)
+            ? UnescapeSJIS(escape([...tripcode].map(toHalfWidthKana).join("")))
+            : tripcode;
+    };
+
+    /**
+     * 12桁トリップ生成
+     */
+    const tripcode12WithKey = (key) => {
+        return btoa(sha1.bin(key))
+            .substr(0, 12)
+            .replace("+", ".");
     };
 
     /**
@@ -95,79 +177,11 @@ sha1.js : http://user1.matsumoto.ne.jp/~goma/js/hash.html
         // Shift_JISバイナリデータに変換
         key = EscapeSJIS(key).replace(shiftJISPercentEncodingPattern, toBinary);
 
-        // 文字数によって分岐する
-        let tripcode = "???";
-        if (key.length >= 12) {
-            // 生キーの可能性がある場合
-            switch (key[0]) {
-                // 10桁トリップの生キー
-                case "#":
-                    const result = key.match(rawKeyPattern);
-                    if (result) {
-                        const digest = result[1];
-                        const salt = (result[2] + "..").substr(0, 2);
-
-                        // キーをバイナリ文字列に変換
-                        key = new Array(8)
-                            .fill()
-                            .map((v, i) => "%" + digest.substr(i * 2, 2))
-                            .join("");
-                        key = unescape(key);
-
-                        // 変換
-                        tripcode = crypt(key, salt);
-
-                        // 生キー付加
-                        if (appendRawKey) {
-                            tripcode += " " + rawKey(digest, salt);
-                        }
-                    }
-                    break;
-
-                // 15桁トリップに変換 (scのみ)
-                case "$":
-                    tripcode = btoa(sha1.bin(key))
-                        .substr(3, 15)
-                        .replace("+", ".")
-                        .replace("/", "!");
-
-                    // キー2文字目が半角カナの場合はトリップを半角カナに置換
-                    if (key.match(halfWidthKanaPattern)) {
-                        tripcode = [...tripcode].map(toHalfWidthKana).join("");
-                        tripcode = escape(tripcode);
-                        tripcode = UnescapeSJIS(tripcode);
-                    }
-                    break;
-
-                // 12桁トリップに変換
-                default:
-                    tripcode = btoa(sha1.bin(key))
-                        .substr(0, 12)
-                        .replace("+", ".");
-            }
-
-        // 10桁トリップに変換
-        } else {
-            // saltを準備
-            let salt = "H.";
-            if (key.length > 0) {
-                salt = [...`${key}${salt}`.substr(1, 2)].map(toSalt).join("");
-            }
-
-            // 変換
-            tripcode = crypt(key, salt);
-
-            // 生キー付加
-            if (appendRawKey) {
-                const digest = [...key]
-                    .map(toDigest)
-                    .concat(new Array(16).fill("0"))
-                    .join("")
-                    .substr(0, 16);
-                tripcode += " " + rawKey(digest, salt);
-            }
-        }
-
-        return tripcode;
+        // 変換
+        return key.length < 12
+            ? tripcodeWithKey(key, appendRawKey)
+            : tripcodeWithRawKey(key, appendRawKey)
+            || tripcode15WithKey(key)
+            || tripcode12WithKey(key);
     };
 })();
